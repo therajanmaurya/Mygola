1ff73211a419c6f05057571d24ed659d22c43669
therajanmaurya <rajanmaurya154@gmail.com>
2015-11-08 18:59:55 +0530

    share sms intent detail activity

--- app/src/main/java/makemytrip/mygola/app/activities/FullActivity.java
+++ app/src/main/java/makemytrip/mygola/app/activities/FullActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -11,7 +12,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.Switch;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
@@ -43,12 +43,11 @@ public class FullActivity extends AppCompatActivity implements RetrievalCallback
 
 	Toolbar toolbar;
 	CollapsingToolbarLayout collapsingToolbar;
-	TextView ratingTextView, pricingTextView, descriptionTextView;
 	ImageView image;
-	Switch favoriteToggle;
 
 	TextView distv ,loctv,citytv,pritv,rattv , destv;
 	RatingBar ratingBar ;
+	FloatingActionButton sharefav , smsfav, favouritefav;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -105,6 +104,37 @@ public class FullActivity extends AppCompatActivity implements RetrievalCallback
 		pritv = (TextView)findViewById(R.id.price_text);
 		destv = (TextView)findViewById(R.id.description_text);
 		ratingBar = (RatingBar)findViewById(R.id.ratingBar);
+
+		sharefav = (FloatingActionButton)findViewById(R.id.share);
+		smsfav = (FloatingActionButton)findViewById(R.id.sms);
+		favouritefav = (FloatingActionButton)findViewById(R.id.favourite);
+
+		sharefav.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent shareIntent = new Intent(Intent.ACTION_SEND);
+				shareIntent.setType("text/plain");
+				shareIntent.putExtra(Intent.EXTRA_SUBJECT,"Mygola.com  " + activity.getName());
+				shareIntent.putExtra(Intent.EXTRA_TEXT, activity.getDescription() + " with Discount " + activity.getDiscount() +
+				" at Effective price: " + "₹ " +activity.getActual_price() + " at Location " + activity.getLocation());
+				startActivity(Intent.createChooser(shareIntent, "Share By Mygola"));
+				FullActivity.this.overridePendingTransition(android.R.anim.slide_in_left, android.R
+						.anim.slide_out_right);
+			}
+		});
+
+		smsfav.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+				Intent smsIntent = new Intent(android.content.Intent.ACTION_VIEW);
+				smsIntent.setType("vnd.android-dir/mms-sms");
+				smsIntent.putExtra("address", "");
+				smsIntent.putExtra("sms_body", " Mygola.com " +activity.getName() +  " :" + activity.getDescription() + " with Discount " + activity.getDiscount() +
+						" at Effective price: " + "₹ " +activity.getActual_price() + " at Location " + activity.getLocation());
+				startActivity(smsIntent);
+			}
+		});
 
 	}
 
